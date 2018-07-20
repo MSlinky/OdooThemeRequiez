3281c0b707bb8d3245c70b9ae561b30144cde510
MSlinky <wwwamrio15@hotmail.com>
2018-07-20 18:33:21 -0500

    v3

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
   'name':'Requiez',
   'description': 'Tema para Grupo Requiez',
-  'version':'0.0.3',
+  'version':'0.0.4',
   'author':'Mario Alberto Solano Zavala',
   'data': [
     'views/assets.xml',
